9c9470336df82dbb4379c40a6a8233833531f098
edgarbp95 <edgarbp95@hotmail.com>
2022-04-06 17:58:41 -0300

    first

---
+++ Ej1/app.js
@@ -0,0 +1,20 @@
+let ingredientes = ["jamon" , "muzzarella" , "morrones" , "harina" , "aceite" , "levadura" , "sal" , "salame" , "oregano" , "maizena"];
+let pares = [];
+let impares = [];
+
+
+const definirIngrediente = ing =>{
+
+    for(let i=0;i<ing.length;i++){
+        if(ing[i].length%2==0){
+           pares.push(ing[i]);
+        } else{
+            impares.push(ing[i])
+        }       
+    }
+}
+
+definirIngrediente(ingredientes);
+
+console.log(`Los elementos pares son: ${pares}`);
+console.log(`Los elementos impares son: ${impares}`);
