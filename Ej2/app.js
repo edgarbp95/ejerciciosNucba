3b1305203936c3905d4d4280b1a29bd595f23cad
edgarbp95 <edgarbp95@hotmail.com>
2022-06-17 16:23:53 -0300

    desafio 4 edu

--- Ej2/app.js
+++ Ej2/app.js
@@ -12,57 +12,68 @@ let nombresPizzas = [];
 let precioPizzas = [];
 let precioDetallado = [];
 
+
 let pizzas = [
     {
         id: 1,
         nombre: "Muzza",
         ingredientes: ["Salsa", "Queso muzza", "Oregano"],
-        precio: 500
+        precio: 500,
+        URL: "https://www.cucinare.tv/wp-content/uploads/2019/03/Queso-en-la-pizza.jpg",
     },
     {
         id: 2,
         nombre: "Napolitana",
         ingredientes: ["Salsa", "Queso muzza", "Tomate", "Oregano"],
-        precio: 599
+        precio: 599,
+        URL:"https://cocinaconlaura.es/wp-content/uploads/2013/10/Pizza-Napolitana-apaisada.jpg"
     }
     ,
     {
         id: 3,
         nombre: "Marinera",
         ingredientes: ["Salsa", "Queso muzza", "Oregano", "Aceite de Oliva", "Albahaca"],
-        precio: 650
+        precio: 650,
+        URL:"https://jetextramar.com/wp-content/uploads/2021/10/receta-de-preparado-para-pizza-Jet-Extramar.jpg"
     },
     {
         id: 4,
         nombre: "Fugazzeta",
         ingredientes: ["Salsa", "Queso muzza", "Cebolla"],
-        precio: 550
+        precio: 550,
+        URL:"https://www.clarin.com/img/2019/09/07/la-fugazzetta-es-un-invento___JhNHCzxP-_1200x630__1.jpg"
     },
     {
         id: 5,
         nombre: "Cuatro Quesos",
         ingredientes: ["Salsa","Muzza", "Gorgonzola","Parmesano", "Fontina"],
-        precio: 700
+        precio: 700,
+        URL:"https://www.supermercedes.com.ar/napoles/wp-content/uploads/2020/04/pizza_cuatro_quesos.jpg"
     },
     {
         id: 6,
         nombre: "Hawaiana",
         ingredientes: ["Salsa","Queso muzza","Ananá","Jamon","Tomate"],
-        precio: 650
+        precio: 650,
+        URL:"https://www.cardamomo.news/__export/1644422142162/sites/debate/img/2022/02/09/pizza_hawaiana_crop1644421835991.jpeg_242310155.jpeg"
     },
     {
         id: 7,
         nombre: "Vegetariana",
         ingredientes: ["Salsa","Calabacin", "Espinacas", "Berenjena"],
-        precio: 650
+        precio: 650,
+        URL:"https://static.americadigital.com/wp-content/uploads/2021/02/america_digital_pizza_vegetariana_faciles_2021-2.jpg"
     },
     {
         id: 8,
         nombre: "Pizza con Pollo",
         ingredientes: ["Salsa","Queso muzza","Pollo","Aceitunas","Champiñones", "Oregano"],
-        precio: 650
+        precio: 650,
+        URL:"http://assets.kraftfoods.com/recipe_images/opendeploy/136150_640x428.jpg"
     }
 ]
+
+localStorage.setItem("pizzas", JSON.stringify(pizzas));
 
 const mostrarPrecio = id=>{
     console.log(`El precio de la ${pizzas[id].nombre} es $${pizzas[id].precio}`)
@@ -100,6 +111,16 @@ botonBuscar.addEventListener("click",()=>{
             nombre.innerHTML = pizzas[pizza].nombre;
             precio.innerHTML = pizzas[pizza].precio;
             nombre.style.color="black";
+            containerCards.innerHTML=`<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <div class="imagen-pizza mb-2"> <img class="card-img-top" src=${pizzas[pizza].URL} alt=""></div> 
+                <h5 class="card-title">${pizzas[pizza].nombre}</h5>
+                <p class="card-text">ID: ${pizzas[pizza].id}</p>
+                <p class="card-text">$${pizzas[pizza].precio}</p>
+                <a href="#" class="btn btn-primary">Ver Pizza</a>
+                </div>
+                </div>
+                </div>`;
             return;
         } else {
             nombre.innerHTML = "Pizza no encontrada";
@@ -119,6 +140,7 @@ const buscarEconomicas= ()=>{
         if(pizzas[pizza].precio < 600){
             containerCards.innerHTML+=`<div class="card" style="width: 18rem;">
                 <div class="card-body">
+                <div class="imagen-pizza mb-2"> <img class="card-img-top" src=${pizzas[pizza].URL} alt=""></div> 
                 <h5 class="card-title">${pizzas[pizza].nombre}</h5>
                 <p class="card-text">ID: ${pizzas[pizza].id}</p>
                 <p class="card-text">$${pizzas[pizza].precio}</p>
@@ -137,6 +159,7 @@ const buscarTodas = ()=>{
     for(let pizza in pizzas){
         containerCards.innerHTML+=`<div class="card" style="width: 18rem;">
         <div class="card-body">
+          <div class="imagen-pizza mb-2"> <img class="card-img-top" src=${pizzas[pizza].URL} alt=""></div> 
           <h5 class="card-title">${pizzas[pizza].nombre}</h5>
           <p class="card-text">ID: ${pizzas[pizza].id}</p>
           <p class="card-text">$${pizzas[pizza].precio}</p>
